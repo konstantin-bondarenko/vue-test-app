419f278d8495d3d502dee5325740e71b3df45478
Konstantin Bondarenko <kosntantinbo92@gmail.com>
2020-06-16 15:36:38 +0300

    create app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app :style="{background:'#E5E5E5'}">
+    <v-app-bar app flat>
+      <span>The Test</span>
+    </v-app-bar>
+
+    <v-content>
+      <Desktop class='hidden-sm-and-down' />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Desktop from '@/components/Desktop'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Desktop
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Desktop.vue
@@ -0,0 +1,37 @@
+<template>
+ <v-container class="fill-height" fluid>
+  <v-row  align="center" justify="center">
+      <v-card flat class="text-xs-center ma-3" width='500' height='500'>
+          <v-card-title class="justify-center">Log In</v-card-title>
+          <span>Please enter your credentials to proceed</span>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                label="Login"
+                v-model="login"
+                type="text"></v-text-field>
+              <v-text-field
+                label="Password"
+                v-model="password"
+                type="password"></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-checkbox :label="'Remember this device'"></v-checkbox>
+          <v-card-actions class="justify-center">
+              <v-btn dark width='100%'>Log in</v-btn>
+          </v-card-actions>
+      </v-card>
+  </v-row>
+ </v-container>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      login: '',
+      password: '',
+    }
+  }
+}
+</script>
