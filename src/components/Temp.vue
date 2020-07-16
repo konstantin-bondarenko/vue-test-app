b46ada3c4533ebd43af8a7b6bf4ea99e47f065ad
Konstantin Bondarenko <kosntantinbo92@gmail.com>
2020-07-17 00:22:36 +0300

    test commit

---
+++ src/components/Temp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="display: flex">
+    <div
+      v-for="name of names"
+      :key="name">
+      <h1>Hello {{ name }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      names: ['Alex', 'Margo', 'Illaoi']
+    }
+  }
+}
+</script>
